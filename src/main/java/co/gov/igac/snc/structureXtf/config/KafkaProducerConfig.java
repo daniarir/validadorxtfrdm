feed1aeb93e02368af54ae40b869e6094ef22c1e
Frando Gonzalez <frando.gonzalez@igac.gov.co>
2022-09-26 22:47:08 +0000

    Updated KafkaProducerConfig.java encedido kafka

--- src/main/java/co/gov/igac/snc/structureXtf/config/KafkaProducerConfig.java
+++ src/main/java/co/gov/igac/snc/structureXtf/config/KafkaProducerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.config.SaslConfigs;
 
 import co.gov.igac.snc.structureXtf.dto.ResponseArchivoDtoKafka;
 
@@ -21,15 +24,29 @@ public class KafkaProducerConfig {
     @Value("${kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${kafka.bootstrap-user}")
+    private String kafkaUser;
+
+    @Value("${kafka.bootstrap-pwd}")
+    private String kafkaPwd;
+
     @Bean
-    public ProducerFactory<String, ResponseArchivoDtoKafka> userProducerFactory() {
+     public ProducerFactory<String, MensajeKafkaDTO> userProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, propiedades.getKafkaServers());
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        //configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT"); 
+        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+       configProps.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
+        configProps.put("acks", "all");
+     configProps.put("retries", 0);
+    configProps.put("batch.size", 16384);
+    configProps.put("linger.ms", 1);
+    configProps.put("buffer.memory", 33554432);
+    configProps.put("sasl.jaas.config","org.apache.kafka.common.security.scram.ScramLoginModule required username='"+kafkaUser+"' password='"+kafkaPwd+"';");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
-
     @Bean
     public KafkaTemplate<String, ResponseArchivoDtoKafka> userKafkaTemplate() {
         return new KafkaTemplate<>(userProducerFactory());
